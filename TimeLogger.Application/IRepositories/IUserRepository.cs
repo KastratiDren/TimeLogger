bea3ee70274cc20f4f030a764e530cdab33e8f25
KastratiDren <drenkastrati33@gmail.com>
2024-12-02 11:25:26 +0100

    feature: create the User repository

---
+++ TimeLogger.Application/IRepositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using TimeLogger.Domain.Entites;
+
+namespace TimeLogger.Application.IRepositories
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByIdAsync(string userId);
+        Task<IEnumerable<User>> GetAllAsync(string email);
+        Task UpdateAsync(User user);
+        Task<bool> DeleteAsync(string userId);
+    }
+}
