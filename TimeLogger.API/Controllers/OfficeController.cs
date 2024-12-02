27f91355a4cd71f22c30be4077386a1ac82ba99f
KastratiDren <drenkastrati33@gmail.com>
2024-12-02 14:49:18 +0100

    feature: add the GetOfficeById endpoint

--- TimeLogger.API/Controllers/OfficeController.cs
+++ TimeLogger.API/Controllers/OfficeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using TimeLogger.Application.Features.Offices.Commands;
 using TimeLogger.Application.Features.Offices.Dtos;
+using TimeLogger.Application.Features.Offices.Queries;
 
 namespace TimeLogger.API.Controllers
 {
@@ -27,5 +28,18 @@ namespace TimeLogger.API.Controllers
 
             return Ok(responseDto);
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOfficeById(int id)
+        {
+            var query = new GetOfficeById(id);
+            var officeDto = await _mediator.Send(query);
+
+            if (officeDto == null)
+                return NotFound();
+
+            return Ok(officeDto);
+        }
+
     }
 }
