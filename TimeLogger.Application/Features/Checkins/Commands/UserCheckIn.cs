ed6b37c7b8c01cc186621ce25051d15053f6aa0a
KastratiDren <drenkastrati33@gmail.com>
2024-12-04 17:03:17 +0100

    feature: add user Check in functionality

---
+++ TimeLogger.Application/Features/Checkins/Commands/UserCheckIn.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using TimeLogger.Application.Features.Checkins.Dto;
+
+namespace TimeLogger.Application.Features.Checkins.Commands
+{
+    public record UserCheckIn(CheckInDto CheckInDto) : IRequest<bool>;
+}
