5b8fd4c49dd30779db6bb5fc74b735dcefc11846
KastratiDren <drenkastrati33@gmail.com>
2024-12-12 10:13:09 +0100

    feature: create the checkout dto

---
+++ TimeLogger.Application/Features/Checkouts/Dtos/CheckOutDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TimeLogger.Application.Features.Checkouts.Dtos
+{
+    public class CheckOutDto
+    {
+        public int OfficeId { get; set; }
+        public string UserId { get; set; } = string.Empty;
+    }
+}
