dd7777c61b5852818d297183dc345e9fc6c93559
KastratiDren <drenkastrati33@gmail.com>
2024-12-12 10:02:56 +0100

    feature: create the checkout repository interface

---
+++ TimeLogger.Application/IRepositories/ICheckOutRepository.cs
@@ -0,0 +1,14 @@
+﻿using TimeLogger.Domain.Entites;
+
+namespace TimeLogger.Application.IRepositories
+{
+    public interface ICheckOutRepository
+    {
+        Task AddAsync(CheckOut checkOut);
+        Task<bool> DeleteAsync(int id);
+        Task<CheckOut?> GetByIdAsync(int id);
+        Task<IEnumerable<CheckOut>> GetAllAsync();
+        Task<IEnumerable<CheckOut>> GetByUserIdAsync(string userId);
+        Task<IEnumerable<CheckOut>> GetByDateRangeAsync(DateTime startDate, DateTime endDate);
+    }
+}
