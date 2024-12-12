e1f3d7963964d8d0c883b93334865502eea4c18b
KastratiDren <drenkastrati33@gmail.com>
2024-12-12 10:15:57 +0100

    feature: create the user checkin command

---
+++ TimeLogger.Application/Features/Checkouts/Commands/UserCheckOut.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using TimeLogger.Application.Features.Checkouts.Dtos;
+
+namespace TimeLogger.Application.Features.Checkouts.Commands
+{
+    public record UserCheckOut(CheckOutDto CheckOutDto) : IRequest<bool>;
+}
