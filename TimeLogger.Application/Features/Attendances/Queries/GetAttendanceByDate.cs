36240200ab5e47882220fdd4b0f661d4e5a06c60
KastratiDren <drenkastrati33@gmail.com>
2024-12-13 17:14:29 +0100

    feature: create the GetAttendanceByDate handler

---
+++ TimeLogger.Application/Features/Attendances/Queries/GetAttendanceByDate.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using TimeLogger.Domain.Entites;
+
+namespace TimeLogger.Application.Features.Attendances.Queries
+{   
+    public record GetAttendanceByDate(DateOnly date) : IRequest<List<Attendance>>;
+}
