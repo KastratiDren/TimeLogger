76a5689a12cb2b5d921806072bbe3f88f6d2142c
KastratiDren <drenkastrati33@gmail.com>
2024-12-13 17:13:49 +0100

    feature: create the GetAttendanceByDate repository method

--- TimeLogger.Application/IRepositories/IAttendanceRepository.cs
+++ TimeLogger.Application/IRepositories/IAttendanceRepository.cs
@@ -5,5 +5,6 @@ namespace TimeLogger.Application.IRepositories
     public interface IAttendanceRepository
     {
         Task<List<Attendance>> GetAttendanceByUserIdAsync(string userId);
+        Task<List<Attendance>> GetAttendanceByDateAsync(DateTime date);
     }
 }
