385005bf1641a90efb26ee5494837a66b5c2b2d3
KastratiDren <drenkastrati33@gmail.com>
2024-12-13 11:00:10 +0100

    feature: create the GetAttendanceByUserId feature

---
+++ TimeLogger.Application/Features/Attendances/Queries/GetAttendanceByUserId.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using TimeLogger.Domain.Entites;
+
+namespace TimeLogger.Application.Features.Attendances.Queries
+{
+    public record GetAttendanceByUserId(string userId) : IRequest<List<Attendance>>;
+}
