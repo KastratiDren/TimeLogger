b0cf8a68811e64d49dfb298e352414c3e92fbd44
KastratiDren <drenkastrati33@gmail.com>
2024-12-16 10:50:11 +0100

    feature: create the AttendanceDTO

---
+++ TimeLogger.Application/Features/Attendances/Dto/AttendanceDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TimeLogger.Application.Features.Attendances.Dto
+{
+    public class AttendanceDto
+    {
+        public DateOnly Date {  get; set; }
+        public string UserId { get; set; }
+        public int OfficeId { get; set; }
+    }
+}
