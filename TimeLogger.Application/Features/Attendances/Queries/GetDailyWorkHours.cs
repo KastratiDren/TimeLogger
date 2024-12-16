05b41dd312cfaf0727c9a2778159b1009fd424af
KastratiDren <drenkastrati33@gmail.com>
2024-12-16 10:50:46 +0100

    feature: create the GetDailyWorkHours handler

---
+++ TimeLogger.Application/Features/Attendances/Queries/GetDailyWorkHours.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using TimeLogger.Application.Features.Attendances.Dto;
+
+namespace TimeLogger.Application.Features.Attendances.Queries
+{
+    public record GetDailyWorkHours(AttendanceDto AttendanceDto) : IRequest<TimeSpan?>;
+}
---
+++ TimeLogger.Application/Features/Attendances/Queries/GetDailyWorkHoursHandler.cs
@@ -0,0 +1,46 @@
+﻿using MediatR;
+using TimeLogger.Application.IRepositories;
+
+namespace TimeLogger.Application.Features.Attendances.Queries
+{
+    public class GetDailyWorkHoursHandler : IRequestHandler<GetDailyWorkHours, TimeSpan?>
+    {
+        private readonly ICheckInRepository _checkInRepository;
+        private readonly ICheckOutRepository _checkOutRepository;
+
+        public GetDailyWorkHoursHandler(ICheckInRepository checkInRepository, ICheckOutRepository checkOutRepository)
+        {
+            _checkInRepository = checkInRepository;
+            _checkOutRepository = checkOutRepository;
+        }
+
+        public async Task<TimeSpan?> Handle(GetDailyWorkHours request, CancellationToken cancellationToken)
+        {
+            var attendanceDto = request.AttendanceDto;
+
+            // Fetch CheckIn for the specified date and office
+            var checkIn = await _checkInRepository.GetCheckinByDateAndOfficeAsync(
+                attendanceDto.Date.ToDateTime(TimeOnly.MinValue),
+                attendanceDto.OfficeId);
+
+            // If there's no CheckIn, return null
+            if (checkIn == null)
+                return null;
+
+            // Fetch CheckOut for the specified date and office
+            var checkOut = await _checkOutRepository.GetCheckoutByDateAndOfficeAsync(
+                attendanceDto.Date.ToDateTime(TimeOnly.MinValue),
+                attendanceDto.OfficeId);
+
+            // If there's no CheckOut, return null
+            if (checkOut == null)
+                return null;
+
+            // Calculate the work duration
+            var workDuration = checkOut.CheckOutTime - checkIn.CheckInTime;
+
+            // Return the time worked
+            return workDuration;
+        }
+    }
+}
