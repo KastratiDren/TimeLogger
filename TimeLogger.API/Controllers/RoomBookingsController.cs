0ae09a599bc664aab614389e58a884f8d93d4485
KastratiDren <drenkastrati33@gmail.com>
2025-01-23 16:42:30 +0100

    chore: refactor the RoomBookingsController

--- TimeLogger.API/Controllers/RoomBookingsController.cs
+++ TimeLogger.API/Controllers/RoomBookingsController.cs
@@ -1,14 +1,10 @@
-﻿using MediatR;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using TimeLogger.Application.Features.RoomBookings.Commands;
+﻿using TimeLogger.Application.Features.RoomBookings.Commands;
 using TimeLogger.Application.Features.RoomBookings.Dtos;
 using TimeLogger.Application.Features.RoomBookings.Queries;
 
 namespace TimeLogger.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/room-bookings")]
     [ApiController]
     public class RoomBookingsController : ControllerBase
     {
@@ -25,25 +21,28 @@ namespace TimeLogger.API.Controllers
             var query = new GetAllRoomBookings();
             var roomBookingListDtos = await _mediator.Send(query);
 
+            if (roomBookingListDtos == null || !roomBookingListDtos.Any())
+                return NotFound(new { Message = "No room bookings found." });
+
             return Ok(roomBookingListDtos);
         }
 
-        [HttpGet("GetRoomBookingsByRoomId/{roomId}")]
+        [HttpGet("room/{roomId}")]
         public async Task<IActionResult> GetRoomBookingsByRoomId(int roomId)
         {
             if (roomId <= 0)
-                return BadRequest("Invalid Room Id");
+                return BadRequest(new { Message = "Invalid Room ID." });
 
             var query = new GetRoomBookings(roomId);
             var roomBookingDetailsDtos = await _mediator.Send(query);
 
             if (roomBookingDetailsDtos == null || !roomBookingDetailsDtos.Any())
-                return NotFound($"No bookings found for Room with Id {roomId}.");
+                return NotFound(new { Message = $"No bookings found for the room with ID {roomId}." });
 
             return Ok(roomBookingDetailsDtos);
         }
 
-        [HttpPost("CreateRoomBooking")]
+        [HttpPost]
         public async Task<IActionResult> CreateRoomBooking([FromBody] RoomBookingsDto roomBookingsDto)
         {
             if (!ModelState.IsValid)
@@ -53,11 +52,9 @@ namespace TimeLogger.API.Controllers
             var success = await _mediator.Send(command);
 
             if (!success)
-                return BadRequest("Room booking could not be created.");
+                return BadRequest(new { Message = "Room booking could not be created." });
 
-            return Ok("Room booking created successfully.");
+            return Ok(new { Message = "Room booking created successfully." });
         }
-
-
     }
 }
